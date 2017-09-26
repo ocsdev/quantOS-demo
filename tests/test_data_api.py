345f552b771b216494f0a85fbcf4e9d27d00674b
bliu <bliu@c05cfc7c-053f-4b68-afce-9d5d402e53a2>
2017-09-26 03:29:52 +0000

    add reference datasource and test
    git-svn-id: svn+ssh://server2/store/svn/quantos/trunk@41 c05cfc7c-053f-4b68-afce-9d5d402e53a2

--- tests/test_data_api.py
+++ tests/test_data_api.py
@@ -1,20 +1,27 @@
 # encoding: UTF-8
 
-from data_api import DataApi
+from jzdataapi.data_api import DataApi
 
 
-if __name__ == "__main__":
+def test_data_api():
     api = DataApi("tcp://10.1.0.210:8910", use_jrpc=False)
-    api.connect()
-
+    api.login('arbitrary_user', "123")
+    
     daily, msg = api.daily(security="600030.SH,000002.SZ", begin_date=20170103, end_date=20170708,
                            fields="open,high,low,close,volume,last,trade_date")
-    daily2, msg = api.daily(security="600030.SH", begin_date=20170103, end_date=20170708,
-                           fields="open,high,low,close,volume,last,trade_date")
-    print type(msg)
-    print "msg = {}".format(msg)
-    print type(daily), len(daily)
-    print daily.columns, daily.shape
-
+    daily2, msg2 = api.daily(security="600030.SH", begin_date=20170103, end_date=20170708,
+                             fields="open,high,low,close,volume,last,trade_date")
+    err_code, err_msg = msg.split(',')
+    assert err_code == '0' and err_msg == ''
+    assert msg2 == '0,'
+    assert daily.shape == (248, 7)
+    assert daily2.shape == (124, 7)
+    
     df, msg = api.bar(security="600030.SH", trade_date=20170904, cycle='1m', begin_time=90000, end_time=150000)
-    print df.columns, df.shape
+    assert df.shape == (240, 14)
+    
+    print "test passed"
+    
+
+if __name__ == "__main__":
+    test_data_api()
