e577c465bc3e217afc68cb3d1d550002360e564b
bliu <bliu@c05cfc7c-053f-4b68-afce-9d5d402e53a2>
2017-09-26 03:29:48 +0000

    encapsulate jzquant api in JzDataserver for backtest
    git-svn-id: svn+ssh://server2/store/svn/quantos/trunk@26 c05cfc7c-053f-4b68-afce-9d5d402e53a2

--- framework/dataserver.py
+++ framework/dataserver.py
@@ -67,10 +67,15 @@ class BaseDataServer(Publisher):
     query(query_type, param, field)
 
     """
-    def __init__(self):
+    # TODO for now, all query functions return DataFrame, they will return custrom data type in later version.
+    # TODO we need a uniform convert from str/int to standard date object, for all derived classes.
+    def __init__(self, name=""):
         Publisher.__init__(self)
 
-        self.source_name = ""
+        if name:
+            self.source_name = name
+        else:
+            self.source_name = str(self.__class__.__name__)
 
     def init_from_config(self, props):
         pass
@@ -78,6 +83,7 @@ class BaseDataServer(Publisher):
     def initialize(self):
         pass
 
+    # TODO deprecated
     def add_batch_subsribe(self, subscriber, securities):
         """
         Add subscriber to multiple securities at once.
@@ -108,6 +114,7 @@ class BaseDataServer(Publisher):
             Call back functions.
 
         """
+        # TODO for now it will not publish event
         for target in targets.split(','):
             sec, data_type = target.split('/')
             if data_type == 'tick':
@@ -116,8 +123,157 @@ class BaseDataServer(Publisher):
                 func = callback['on_bar']
             self.add_subscriber(func, target)
 
+    @abstractmethod
     def quote(self, security, field):
         pass
+
+    @abstractmethod
+    def daily(self, security, begin_date, end_date, field="", fq=None, skip_paused=False):
+        """
+        Query dar bar,
+        support auto-fill suspended securities data,
+        support auto-adjust for splits, dividends and distributions.
+
+        Parameters
+        ----------
+        security : str
+            support multiple securities, separated by comma.
+        begin_date : int (YYYMMDD) or str ('YYYY-MM-DD')
+        end_date : int (YYYMMDD) or str ('YYYY-MM-DD')
+        field : separated by comma ','
+            Default is all fields included.
+        fq : {'pre', 'post', None}
+            How to adjust price. None for no adjust.
+        skip_paused : bool
+            True for filter out those suspended,
+            False for auto fill using last trade date's close price.
+
+        Returns
+        -------
+        df : dict of {security: pd.DataFrame}
+            columns:
+                security, code, trade_date, open, high, low, close, volume, turnover, vwap, oi, suspended
+
+        Examples
+        --------
+        df, msg = api.daily("00001.SH,cu1709.SHF",begin_date=20170503, end_date=20170708,
+                            field="open,high,low,last,volume", fq=None, skip_suspended=True)
+
+        """
+        pass
+
+    @abstractmethod
+    def bar(self, security, begin_time=None, end_time=None, trade_date=None, field="", cycle='1m'):
+        """
+        Query minute bars of various type, return DataFrame.
+
+        Parameters
+        ----------
+        security : str
+            support multiple securities, separated by comma.
+        begin_time : int (HHMMSS) or str ('HH:MM:SS')
+            Default is market open time.
+        end_time : int (HHMMSS) or str ('HH:MM:SS')
+            Default is market close time.
+        trade_date : int (YYYMMDD) or str ('YYYY-MM-DD')
+            Default is current trade_date.
+        field : separated by comma ','
+            Default is all fields included.
+        cycle : framework.common.MINBAR_TYPE {'1m', '5m', '15m'}
+            Minute bar type, default is '1m'
+
+        Returns
+        -------
+        df : dict of {security: pd.DataFrame}
+            columns:
+                security, code, date, time, trade_date, cycle, open, high, low, close, volume, turnover, vwap, oi
+
+        Examples
+        --------
+        df, msg = api.bar("000001.SH,cu1709.SHF", begin_time="09:56:00", end_time="13:56:00",
+                          trade_date="20170823", field="open,high,low,last,volume", cycle="5m")
+
+        """
+        # TODO data_server DOES NOT know "current date".
+        pass
+
+    @abstractmethod
+    def tick(self, security, begin_time=None, end_time=None, trade_date=None, field=""):
+        pass
+
+    @abstractmethod
+    def query(self, query_type, param, field):
+        pass
+
+    @abstractmethod
+    def get_split_dividend(self):
+        pass
+
+
+class JzDataServer(BaseDataServer):
+    """
+    JzDataServer uses data from jz's local database.
+
+    """
+    # TODO no validity check for input parameters
+
+    def __init__(self):
+        BaseDataServer.__init__(self)
+
+        address = 'tcp://10.2.0.14:61616'
+        usr, pwd = "TODO", "TODO"
+        self.api = jzquant_api.get_jzquant_api(address, usr, pwd)
+
+    @staticmethod
+    def _to_int(dt):
+        """dt is int or str"""
+        if isinstance(dt, (str, unicode)):
+            year, month, day = dt[:4], dt[5: 7], dt[8: ]
+            return int(year) * 10000 + int(month) * 100 + int(day)
+        elif isinstance(dt, (int, long)):
+            return dt
+        else:
+            raise NotImplementedError("Only support int or str of certain format")
+
+    @staticmethod
+    def _to_str(dt):
+        """int to str"""
+        year = str(dt // 10000)
+        month = str(dt // 100 % 100)
+        day = str(dt % 100)
+        return '-'.join([year, month, day])
+
+    def daily(self, security, begin_date, end_date, field="", fq=None, skip_paused=False):
+        # convert to int format YYYYMMDD
+        begin_date = self._to_int(begin_date)
+        end_date = self._to_int(end_date)
+        # convert to str 'YYYY-MM-DD' (only for jsd api)
+        begin_date = self._to_str(begin_date)
+        end_date = self._to_str(end_date)
+
+        securities = security.split(',')
+        d = dict()
+        for sec in securities:
+            df, err_msg = self.api.jz_unified('jsd', sec, fields=field, format_='pandas',
+                                              start_date=begin_date, end_date=end_date)
+            if err_msg:
+                d[sec] = err_msg
+            else:
+                d[sec] = df
+        return d
+
+    def bar(self, security, begin_time=None, end_time=None, trade_date=None, field="", cycle='1m'):
+        securities = security.split(',')
+        d = dict()
+        for sec in securities:
+            df, err_msg = self.api.jz_unified('jsh', sec, fields=field, format_='pandas',
+                                              date=trade_date, start_time=begin_time, end_time=end_time,
+                                              bar_size=cycle.replace('m', ''))
+            if err_msg:
+                d[sec] = err_msg
+            else:
+                d[sec] = df
+        return d
 
 
 class DataServer(Publisher):
@@ -261,22 +417,37 @@ class JshHistoryBarDataServer(DataServer):
         return None
 
 
-# 直接运行脚本可以进行测试
-if __name__ == '__main__':
-    
+def test_old():
     props = dict()
     props['jsh.addr'] = 'tcp://10.2.0.14:61616'
     props['bar_type'] = common.QUOTE_TYPE.MIN
     props['symbol'] = '600030.SH'
-    
+
     server = JshHistoryBarDataServer()
     server.init_from_config(props)
-    
+
     server.initialize()
-    
+
     server.subscribe(None, ['600030.SH'])
 
     quotes = server.get_daily_quotes(20170712)
     for quote in quotes:
         print quote.symbol, quote.time, quote.open, quote.high
 
+
+def test_new():
+    ds = JzDataServer()
+    res = ds.daily('rb1710.SHF,600662.SH', '2017-08-28', 20170831, "")
+    assert res['rb1710.SHF'].shape == (4, 12)
+    assert res['600662.SH'].ix[0, 'VOLUME'] == 7174813.00
+
+    res2 = ds.bar('rb1710.SHF,600662.SH', '', '', 20170831, '', '1m')
+    assert res2['rb1710.SHF'].shape == (345, 10)
+    assert res2['600662.SH'].shape == (240, 10)
+
+    print "Test passed."
+
+# 直接运行脚本可以进行测试
+if __name__ == '__main__':
+    # test_old()
+    test_new()
