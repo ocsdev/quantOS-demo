95eef51700394e8201b6a87c0f25e22fab6566be
bliu <bliu@c05cfc7c-053f-4b68-afce-9d5d402e53a2>
2017-09-26 03:29:46 +0000

    add get_next_period_day function
    git-svn-id: svn+ssh://server2/store/svn/quantos/trunk@14 c05cfc7c-053f-4b68-afce-9d5d402e53a2

--- framework/jzcalendar.py
+++ framework/jzcalendar.py
@@ -1,38 +1,94 @@
+# encoding: utf-8
 
 from dbmanager import *
-import MySQLdb
+# import MySQLdb
 import pandas as pd
-import numpy as np
+import datetime
+from pandas.tseries import offsets
 
 
 class JzCalendar(object):
-    
-    #----------------------------------------------------------------------
-    def __init__(self):               
-        self.conn = getJztsConnection()
+    """
+    A calendar for manage trade date.
+
+    """
+    def __init__(self):
+        self.conn = get_jzts_connection()
         
-    def getPreTradeDate(self, date):
+    def get_last_trade_date(self, date):
         sql = 'select max(date) as date from Calendar where date < %d'%(date)
         df = pd.read_sql(sql, self.conn)
         return df['date'][0]
     
-    def getNextTradeDate(self, date):
+    def get_next_trade_date(self, date):
         sql = 'select min(date) as date from Calendar where date > %d'%(date)
         df = pd.read_sql(sql, self.conn)       
         return df['date'][0]
-    
-    def getTradeDates(self, begin, end):
+
+    @staticmethod
+    def get_next_period_day(current, period, n):
+        """
+        Get the n'th day in next period from current day.
+
+        Parameters
+        ----------
+        current : int
+            Current date in format "%Y%m%d".
+        period : str
+            Interval between current and next. {'day', 'week', 'month'}
+        n : int
+            n'th business day after next period.
+
+        Returns
+        -------
+        nxt : int
+
+        """
+        current_dt = JzCalendar.convert_int_to_datetime(current)
+        if period == 'day':
+            offset = offsets.BDay()  # move to next business day
+        elif period == 'week':
+            offset = offsets.Week(weekday=0)  # move to next Monday
+        elif period == 'month':
+            offset = offsets.BMonthBegin()  # move to first business day of next month
+        else:
+            raise NotImplementedError("Frequency as {} not support".format(period))
+
+        next_dt = current_dt + offset
+        if n:
+            next_dt = next_dt + n * offsets.BDay()
+        nxt = JzCalendar.convert_datetime_to_int(next_dt)
+        return nxt
+
+    def get_trade_date_range(self, begin, end):
         sql = 'select date from Calendar where date >= %d  and date <= %d'%(begin,end)
         df = pd.read_sql(sql, self.conn)
         return df['date']
-    
-    def transferDtToInt(self, now):
-        return now.year * 10000 + now.month * 100 + now.day
- 
- #### for testing   
+
+    @staticmethod
+    def convert_int_to_datetime(dt):
+        """Convert int date (%Y%m%d) to datetime.datetime object."""
+        return datetime.datetime.strptime(str(dt), "%Y%m%d")
+
+    @staticmethod
+    def convert_datetime_to_int(dt):
+        return dt.year * 10000 + dt.month * 100 + dt.day
+
+
 if __name__ == '__main__':
+    """for testing"""
     calendar = JzCalendar()
     date = 20170808
-    print calendar.getPreTradeDate(date)
-    print calendar.getNextTradeDate(date)
-    print calendar.getTradeDates(20170701, 20170723)
+    print calendar.get_last_trade_date(date)
+    print calendar.get_next_trade_date(date)
+    print calendar.get_trade_date_range(20170701, 20170723)
+    assert calendar.get_next_period_day(20170831, 'day', 1) == 20170904
+    assert calendar.get_next_period_day(20170831, 'week', 1) == 20170905
+    assert calendar.get_next_period_day(20170831, 'month', 0) == 20170901
+
+    monthly = 20170101
+    while monthly < 20180301:
+        monthly = calendar.get_next_period_day(monthly, 'month', 0)
+        assert datetime.datetime.strptime(str(monthly), "%Y%m%d").weekday() < 5
+
+    print "Test passed."
