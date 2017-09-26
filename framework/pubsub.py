14ea52ea79ff09c0d762fd21f1a83f6c13029715
bliu <bliu@c05cfc7c-053f-4b68-afce-9d5d402e53a2>
2017-09-26 03:29:46 +0000

    use default dict and improve performance
    git-svn-id: svn+ssh://server2/store/svn/quantos/trunk@15 c05cfc7c-053f-4b68-afce-9d5d402e53a2

--- framework/pubsub.py
+++ framework/pubsub.py
@@ -1,36 +1,47 @@
 # encoding: UTF-8
 
 from abc import abstractmethod
+from collections import defaultdict
 
 
-########################################################################
+class Event(object):
+    def __init__(self, topic="unknown", data=None):
+        self.data = data
+        self.topic = topic
+
+
 class Publisher(object):
 
-    #----------------------------------------------------------------------
     def __init__(self):
-        self.subscribers = {}
+        self.subscribers = defaultdict(list)  # use empty list as default-factory
 
-    
-    def onSubscribe(self, subscriber, topic):
-        if (self.subscribers.has_key(topic)):
-            self.subscribers.get(topic).append(subscriber)
-        else:
-            self.subscribers[topic] = []
-            self.subscribers.get(topic).append(subscriber)
-            
-    def publish(self, event, topic):
-        if (self.subscribers.has_key(topic)):
-            sublist = self.subscribers.get(topic)
-            for i in xrange(len(sublist)):
-                sublist[i].onEvent(event, topic)
-    
-    def getTopic(self):
+    def add_subscriber(self, subscriber, topic):
+        self.subscribers[topic].append(subscriber)
+
+    def publish(self, event):
+        """
+        Publish an event to all its subscribers.
+
+        Parameters
+        ----------
+        event : Event object
+
+        Returns
+        -------
+
+        """
+        sub_list = self.subscribers.get(event.topic, None)
+        if sub_list is None:
+            return
+
+        for subscriber in sub_list:
+            subscriber.on_event(event)
+
+    def get_topics(self):
         return self.subscribers.keys()
     
                 
 class Subscriber(object):
-    
-    #----------------------------------------------------------------------
     def __init__(self):
         pass
     
@@ -39,7 +50,18 @@ class Subscriber(object):
         pass
     
     @abstractmethod
-    def onEvent(self, event, topic):
+    def on_event(self, event):
+        """
+        Process an event.
+
+        Parameters
+        ----------
+        event : Event object
+
+        """
         pass
-    
-    
+
+
+if __name__ == "__main__":
+    p = Publisher()
+    p.add_subscriber(lambda x: x, 'topic1')
