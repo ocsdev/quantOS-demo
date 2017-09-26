71fb0e34f466faa6534c9c964b957f40fc31676a
bliu <bliu@c05cfc7c-053f-4b68-afce-9d5d402e53a2>
2017-09-26 03:29:47 +0000

    return int instead of str, add __init__.py
    git-svn-id: svn+ssh://server2/store/svn/quantos/trunk@21 c05cfc7c-053f-4b68-afce-9d5d402e53a2

--- util/sequence.py
+++ util/sequence.py
@@ -9,16 +9,16 @@ class SequenceGenerator(object):
 
     def get_next(self, key):
         self.__d[key] += 1
-        return str(self.__d[key])
+        return self.__d[key]
 
 
 if __name__ == "__main__":
     sg = SequenceGenerator()
     for i in range(1, 999):
-        assert sg.get_next('order') == str(i)
+        assert sg.get_next('order') == i
 
     text = 'trade'
     sg.get_next(text)
     sg.get_next(text)
     for i in range(3, 999):
-        assert sg.get_next(text) == str(i)
+        assert sg.get_next(text) == i
