f53e55f941969fb61dc8f9df976f31a8d4070f38
brillliantz <brillliantz@outlook.com>
2017-10-10 21:39:48 +0800

    add tests

--- quantos/util/sequence.py
+++ quantos/util/sequence.py
@@ -10,15 +10,3 @@ class SequenceGenerator(object):
     def get_next(self, key):
         self.__d[key] += 1
         return self.__d[key]
-
-
-if __name__ == "__main__":
-    sg = SequenceGenerator()
-    for i in range(1, 999):
-        assert sg.get_next('order') == i
-    
-    text = 'trade'
-    sg.get_next(text)
-    sg.get_next(text)
-    for i in range(3, 999):
-        assert sg.get_next(text) == i
